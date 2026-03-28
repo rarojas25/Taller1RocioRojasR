1910464f2055bb96feb0cf52b84d627c3c1bcdbc
rarojas25 <rocio.rojas02@alumnos.ucn.cl>
2026-03-27 21:51:33 -0300

    add Definición de arreglos de usuarios y registros

--- Taller/logica/Main.java
+++ Taller/logica/Main.java
@@ -1,8 +1,28 @@
+/*Integrante 1: Rocío Azucena Rojas Robledo - 21.694.049 - 0 - rarojas25
+  https://github.com/rarojas25/Taller1RocioRojasR 
+ */
+
 package logica;
 
-public class Main{
+import java.util.Scanner;
 
-	public static void main(String[]args) {
+public class Main{
+	
+		static String[] nombres = new String[50];
+		static String[] claves = new String[50];
+		static int totalUsuarios = 0;
+		
+		static String[] usuarioReg = new String[300];
+		static String[] fechaReg = new String[300];
+		static int[] horasReg = new int[300];
+		static String[] actividadReg = new String[300];
+		static int totalReg = 0;
+		
+		static Scanner teclado = new Scanner(System.in); 
+		
+		
+		public static void main(String[]args) {
+			
 		
 	}
 }
